6efff329f44d14edf58d49d5b771965ee77c5aef
mrsky1001 <nikita.nk16@yandex.ru>
2021-05-05 20:00:31 +0600

    add css inputs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import "./App.css"
 import React from "react"
 import DemoForm from "../DemoForm/DemoForm";
 
--- src/components/DemoForm/DemoForm.js
+++ src/components/DemoForm/DemoForm.js
@@ -6,14 +6,51 @@ const DemoForm = () => {
     const [firstName, setFirstName] = useState("")
     const [lastName, setLastName] = useState("")
     const [middleName, setMiddleName] = useState("")
+    const [phone, setPhone] = useState("")
+    const [country, setCountry] = useState("")
 
+    const countries = ['Таиланд', 'Индонезия', 'Малайзия', 'Вьетнам']
 
     return (
         <div className={'demo-form'}>
+            <h2>Demo Form</h2>
             <form className={"form"}>
-                <input value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
-                <input value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
-                <input value={middleName} onChange={(e)=>setMiddleName(e.target.value)}/>
+                <section className={"form-row"}>
+                    <input
+                        placeholder={"Фамилия"}
+                        className={"form-control"}
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}/>
+                    <input
+                        placeholder={"Имя"}
+                        className={"form-control"}
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}/>
+                    <input
+                        placeholder={"Фамилия"}
+                        className={"form-control"}
+                        value={middleName}
+                        onChange={(e) => setMiddleName(e.target.value)}/>
+                </section>
+                <section className={"form-row"}>
+                    <input
+                        placeholder={"Телефон"}
+                        className={"form-control"}
+                        type={"phone"}
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}/>
+                    <select
+                        placeholder={"Страна"}
+                        className={"form-control form-select"}
+                        value={country}
+                        onChange={(e) => setCountry(e.target.value)}
+                    >
+                        {countries.map((country, idx) =>
+                            <option key={idx}>{country}</option>
+                        )}
+                    </select>
+                </section>
+
             </form>
         </div>
     )
