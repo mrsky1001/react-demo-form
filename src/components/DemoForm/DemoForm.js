2afccf9ba41dfa954558bb9e45076b56fc22c35e
mrsky1001 <nikita.nk16@yandex.ru>
2021-05-05 20:58:07 +0600

    add css inputs

--- src/components/DemoForm/DemoForm.js
+++ src/components/DemoForm/DemoForm.js
@@ -1,6 +1,7 @@
 import "./DemoForm.css"
 import React from "react"
 import {useState} from "react";
+import LoadingSymbol from "../LoadingSymbol/LoadingSymbol";
 
 const DemoForm = () => {
     const [firstName, setFirstName] = useState("")
@@ -8,25 +9,43 @@ const DemoForm = () => {
     const [middleName, setMiddleName] = useState("")
     const [phone, setPhone] = useState("")
     const [country, setCountry] = useState("")
-
     const countries = ['Таиланд', 'Индонезия', 'Малайзия', 'Вьетнам']
+    const [loading, setLoading] = useState(false)
+    const [isSentForm, setIsSentForm] = useState(false)
+
+    const submitForm = () => {
+        setLoading(true)
+        setIsSentForm(true)
+    }
 
     return (
         <div className={'demo-form'}>
             <h2>Demo Form</h2>
-            <form className={"form"}>
+            <form
+                id={"demo-form"}
+                action={"https://localhost/form"}
+                onSubmit={submitForm}
+                className={"form"}
+                method={"post"}
+            >
                 <section className={"form-row"}>
                     <input
+                        form={"demo-form"}
+                        name={"lastName"}
                         placeholder={"Фамилия"}
                         className={"form-control"}
                         value={lastName}
                         onChange={(e) => setLastName(e.target.value)}/>
                     <input
+                        form={"demo-form"}
+                        name={"firstName"}
                         placeholder={"Имя"}
                         className={"form-control"}
                         value={firstName}
                         onChange={(e) => setFirstName(e.target.value)}/>
                     <input
+                        form={"demo-form"}
+                        name={"middleName"}
                         placeholder={"Фамилия"}
                         className={"form-control"}
                         value={middleName}
@@ -34,12 +53,16 @@ const DemoForm = () => {
                 </section>
                 <section className={"form-row"}>
                     <input
+                        form={"demo-form"}
+                        name={"phone"}
                         placeholder={"Телефон"}
                         className={"form-control"}
-                        type={"phone"}
+                        type={"number"}
                         value={phone}
                         onChange={(e) => setPhone(e.target.value)}/>
                     <select
+                        form={"demo-form"}
+                        name={"country"}
                         placeholder={"Страна"}
                         className={"form-control form-select"}
                         value={country}
@@ -49,6 +72,21 @@ const DemoForm = () => {
                             <option key={idx}>{country}</option>
                         )}
                     </select>
+                </section>
+                <section className={"form-actions-row"}>
+                    {isSentForm ?
+                        <p className={"form-control form-success-message"}> Форма успешно отправлена!</p>
+                        : loading ? <LoadingSymbol/>
+                            : <>
+                                <div className={"form-control empty-control"}/>
+                                <button
+                                    form={"demo-form"}
+                                    className={"form-control form-button"}
+                                    type={"submit"}>
+                                    Отправить
+                                </button>
+                            </>
+                    }
                 </section>
 
             </form>
